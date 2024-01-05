2db548aac1663165cd28e069ce67e05a49ba3079
isarahchung <sarah00chung@gmail.com>
2024-01-05 05:13:33 -0800

    created App folder

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, KeyboardAvoidingView} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
+      <Text>Nice!</Text>
       <StatusBar style="auto" />
     </View>
   );
